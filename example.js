fe58f2ceb4b088c637c5941bd9415e0ae5501842
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-06-22 15:06:11 +0700

    update Plugin

--- example.js
+++ example.js
@@ -21,20 +21,46 @@ const data = [
 	}
     ],
     clearMe: "text"
+  },
+  {
+    title : "title1",
+    description: "description1",
+    blog: "This is a blog.",
+    date: "11/4/2013",
+    extra : {
+	link : {
+	  linka : 'http://google.com'
+	}
+    },
+    list1:[
+	{
+	    name:"mike"
+	}
+    ],
+    list2:[
+	{
+	    item: "thing"
+	}
+    ],
+    clearMe: "text"
   }
+
 ]
 
 
 const format = {
-  Title: 'title||description',
-  Link: 'extra.link.linka',
-  Hello: 'blog.a||blog',
-  name: 'list1',
-  clear: 'xxx'
+  Title: {
+    field: 'title||description',
+    defaultValue: 'Default' 
+  },
+  Link: { field: 'extra.link.linka' },
+  Hello: { field: 'blog.a||blog' },
+  name: { field: 'list1' },
+  clear: { 
+    field: 'xxx',
+    defaultValue: [10,90]
+  },
+  test: { field: 'sd' }
 }
 
-const defaultValue = {
-  Title: 'Test'
-}
-
-console.log(jsonTranform(data, format, defaultValue))
+console.log(jsonTranform(data, format))
--- lib.js
+++ lib.js
@@ -1,15 +1,16 @@
-const jsonTranform = function jsonTranform(data, format, defaultVale) {
+const jsonTranform = function jsonTranform(data, format) {
   return data.map((obj, index) => {
     const data = Object.keys(format).reduce((result, field, index) => {
-      const orPath = format[field].split('||').reverse()
+      const orPath = (format[field].field).split('||').reverse()
       const orPathResult = orPath.reduce((value, field) => {
         const pathIndex = field.split('.')
         const returnValue = pathIndex.reduce((obj, key) => {
 	  return obj[key]
         },obj)
 	return returnValue ? returnValue : value
-      }, undefined)      
-      const json =  { [field] : orPathResult}
+      }, undefined)     
+      const finalValue = orPathResult ? orPathResult : format[field].defaultValue
+      const json =  { [field] : finalValue}
       return Object.assign(result, json)
     },{})
     return data
