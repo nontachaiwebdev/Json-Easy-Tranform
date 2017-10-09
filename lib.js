3259612d64ce41d81bc93442a769ca5b89122fd5
botbotbot <tkroputa@gmail.com>
2017-10-09 15:14:26 +0700

    using function instead arrow function
    to avoid  UglifyJs:: SyntaxError: Unexpected token: operator (>) on Webpack v1

--- lib.js
+++ lib.js
@@ -5,21 +5,22 @@ const jsonTranform = function jsonTranform(data, format) {
   return data.constructor === Array ? tranformFunc(data) : tranformFunc(new Array(data))[0] 
 }
 
-const tranform = (format) => (data) => {
-  return data.map((obj, index) => {
-    const data = Object.keys(format).reduce((result, field, index) => {
-      const arg = (typeof format[field] === 'object') ? format[field].field : format[field]
-      const orPath = arg.split('||').reverse()
-      const orPathResult = orPath.reduce((value, field) => {
-	const getValue = _.get(obj, field)
-	return getValue ? getValue : value
-      }, undefined)     
-      const finalValue = orPathResult ? orPathResult : format[field].defaultValue
-      const json =  { [field] : finalValue}
-      return Object.assign(result, json)
-    },{})
-    return data
-  })
+const tranform = function (format) {
+  return function(data) {
+    return data.map((obj, index) => {
+      const data = Object.keys(format).reduce((result, field, index) => {
+        const arg = (typeof format[field] === 'object') ? format[field].field : format[field]
+        const orPath = arg.split('||').reverse()
+        const orPathResult = orPath.reduce((value, field) => {
+	  const getValue = _.get(obj, field)
+	  return getValue ? getValue : value
+        }, undefined)     
+        const finalValue = orPathResult ? orPathResult : format[field].defaultValue
+        const json =  { [field] : finalValue}
+        return Object.assign(result, json)
+      },{})
+      return data
+    })
 }
 
 module.exports = jsonTranform
